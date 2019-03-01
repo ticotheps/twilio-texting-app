ba64e59b97de86994c018150f2173b34fc1b4260
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-01 18:50:45 -0500

    Created 'require' statements for dependencies in index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 // Install Dependencies with 'yarn add express cors twilio'
+
+const express = require('express');
+const cors = require('cors');
+const twilio = require('twilio');
+
+// Twilio API requirements
+const accountSid = 
