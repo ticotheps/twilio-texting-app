85e1eac6ac83d6fe3065b053bc8c218692d7e174
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-04 19:13:27 -0500

    Created .env file to hold variables with private information

--- server/index.js
+++ server/index.js
@@ -4,5 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const twilio = require('twilio');
 
+// Installed dependency for dotenv to hold environment variable
+require('dotenv').config();
+
 // Twilio API requirements
-const accountSid = 
+const accountSid = MY_ACC_SID;
+const authToken = MY_AUTH_TOKEN;
