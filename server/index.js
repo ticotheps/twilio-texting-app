5b3efa41fc6a12b09d8e3b313533331bae5b2203
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-04 19:57:14 -0500

    Refactored the index.js file inside of server folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 // Installed dependency for dotenv to hold environment variable
-require('dotenv').config();
+const dotenv = require('dotenv');
 
 // Install Dependencies with 'yarn add express cors twilio'
 const express = require('express');
 const cors = require('cors');
 const twilio = require('twilio');
 
+
 // Twilio API requirements
 const accountSid = MY_ACC_SID;
 const authToken = MY_AUTH_TOKEN;
+const client = (accountSid, authToken);
+
+const app = express(); // alias
+
+app.use(cors()); // blocks the browser from restricting any data
