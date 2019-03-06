c124ad5cac1de3e787ec04f6c31cc5f04016274e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-06 02:09:11 -0500

    Imported font awesome library via <link> inside of index.html and removed unnecessary borders that were used for styling purposes

--- src/App.js
+++ src/App.js
@@ -11,34 +11,45 @@ class App extends Component {
 
   handleChangesInput = e => {
     const { text } = this.state;
-    console.log("The handleChangesInput() function was triggered!");
+    // console.log("The handleChangesInput() function was triggered!");
     e.preventDefault();
     this.setState({ text: {...text, recipient: e.target.value}});
   }
 
   handleChangesTextarea = e => {
     const { text } = this.state;
-    console.log("The handleChangesTextarea() function was successful!");
+    // console.log("The handleChangesTextarea() function was successful!");
     e.preventDefault();
     this.setState({ text: {...text, textmessage: e.target.value}});
   }
 
   sendText = _ => {
-    console.log("The sendText() function just fired off!");
+    console.log("Your text message was sent successfully!");
     const { text } = this.state;
     //pass variables within the query string
     fetch(`http://192.168.1.14:4000/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
+
+    this.setState({
+      text: {
+        recipient: '',
+        textmessage: ''
+      }
+    });
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App-container">
         <h1 className="App-title">Welcome to Tico's *PROTOTYPE*<br/>Hooper Texting App!</h1>
         <div className="App-content">
-          <h2>Send a "Bat Signal" Text!</h2>
+          <div className="App-header">
+            <i className="fas fa-basketball-ball"></i>
+            <h2 className="App-heading">Send a Text to the Hoop Squad!</h2>
+            <i className="far fa-comments"></i>
+          </div>
           <div className="container-recipient">
-            <label className="label-recipient">Recipient's Phone Number:</label>
+            <label className="label-recipient">Hooper's Phone Number:</label>
             <input 
               className="input-recipient"
               type="text"
