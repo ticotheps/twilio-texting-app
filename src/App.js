99a7a2c7d3d63ca369cbf3048e28b49aad237316
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-05 23:31:58 -0500

    Added <h1> styles and .App styles to App.css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: {
+      recipient: '',
+      textmessage: ''
+    }
+  };
+
+  sendText = _ => {
+    const { text } = this.state;
+    //pass variables within the query string
+    fetch(`http://localhost:4000/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
+    .catch(err => console.error(err))
+  }
+
   render() {
+    const { text } = this.state;
+
+    const space = {
+      margin: 8
+    };
+
+    const textArea = {
+      borderRadius: 4
+    };
+
     return (
       <div className="App">
-        <h1>Welcome to Tico's Texting App!</h1>
+        <h1 className="welcome-title">Welcome to Tico's Texting App!</h1>
+        <div>
+
+        </div>
       </div>
     );
   }
