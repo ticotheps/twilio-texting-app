616864321809d30063ea90606bd1c56c45b5b0fb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-06 21:17:16 -0500

    Updated const twilio variable

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 // Install Dependencies with 'yarn add express cors twilio'
 const express = require('express');
 const cors = require('cors');
-
+const twilio = require('twilio');
 
 // Twilio API requirements
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
