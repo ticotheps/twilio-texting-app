b928bb973e55c9e07f8352b8d2e05935f4685834
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 12:17:27 -0400

    Refactors app.js code

--- chatbot/app.js
+++ chatbot/app.js
@@ -1,15 +1,16 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
-const twilio = require('twilio');
+const twilio = require("twilio");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = new twilio(accountSid, authToken);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-mongoose.connect('mongodb+srv://tico:<password>@tico-chatbot-y60so.mongodb.net/test?retryWrites=true')
-
+app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect(
+  "mongodb+srv://tico:<password>@tico-chatbot-y60so.mongodb.net/test?retryWrites=true"
+);
