de96068041fe61016fa83ac03dddb7e3cd8acf1e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 11:47:54 -0400

    Creates .env file to hold twilio accountSid and authToken

--- chatbot/app.js
+++ chatbot/app.js
@@ -5,4 +5,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const twilio = require('twilio');
-const accountSid = ''
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
