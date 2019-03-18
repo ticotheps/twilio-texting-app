dbd402f66dc0d161f0b12facf7de0159e4254579
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-18 12:04:37 -0400

    Imports dependencies and passwords into app.js

--- chatbot/app.js
+++ chatbot/app.js
@@ -8,3 +8,8 @@ const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = new twilio(accountSid, authToken);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+mongoose.connect('mongodb+srv://tico:<password>@tico-chatbot-y60so.mongodb.net/test?retryWrites=true')
+
